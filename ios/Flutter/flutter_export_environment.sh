abf79c88f6018ed7e73016e360ba75c7f78bc40b
SilentCaMXMF <silentcamxmf@users.noreply.github.com>
2026-03-02 22:00:27 +0000

    chore: add web platform support for Flutter web build
    - Created web/ folder with index.html and manifest
    - Updated iOS Flutter configuration

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/pedroocalado/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/pedroocalado/MotorBike_Parking_App/MotorBike-Parking-App"
+export "FLUTTER_APPLICATION_PATH=/home/pedroocalado/dev/Projects/MotorBike_Parking_App/MotorBike-Parking-App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
